8312ceb07f8d113fc9d0125cf05c3dd02da99d9d
HeriSanDev <herisilvasantos@outlook.com>
2022-03-07 22:26:23 -0300

    Criado site simples de urna digital

---
+++ etapas.js
@@ -0,0 +1,69 @@
+let etapas=[
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '44555',
+                name: 'Ivonete da Silva',
+                partido:'PNC',
+                fotos:[
+                    {url:'imagens/v-44555.png',legenda: 'Vereador', small: false}
+                ]
+            },
+            {   
+                numero: '55333',
+                name: 'Mauro Indigno',
+                partido: 'PPG',
+                fotos:[
+                    {url:'imagens/v-55333.png',legenda: 'Vereador', small: false}
+                ]
+            },
+            {   
+                numero: '66444',
+                name: 'Julio Sacarolha',
+                partido: 'PSD',
+                fotos:[
+                    {url:'imagens/v-66444.png',legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '45',
+                name: 'Jairo Bolsonabo',
+                partido:'PPG',
+                vice: 'Luizio Lalo da Silva',
+                fotos:[
+                    {url:'imagens/p-45.png',legenda: 'Prefeito'},
+                    {url:'imagens/vp-45.png',legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {   
+                numero: '91',
+                name: 'João Doriana',
+                partido: 'PSD',
+                vice: "José Maoleve",
+                fotos:[
+                    {url:'imagens/p-91.png',legenda: 'Prefeito'},
+                    {url:'imagens/vp-91.png',legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {   
+                numero: '53',
+                name: 'Vlademir Vampiro Souza',
+                partido: 'PNC',
+                vice: 'Jose Nimguem',
+                fotos:[
+                    {url:'imagens/p-53.png',legenda: 'Prefeito'},
+                    {url:'imagens/vp-53.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]   
+    }
+]
